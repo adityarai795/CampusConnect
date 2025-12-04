4040da1a7a0291f4d039160a99d6a09bb5d38c3a
aditya Rai <adityrai742@gmail.com>
2025-12-04 16:20:42 +0530

    refactor: format baseURL declarations for consistency in API files

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 // export const BASE_URL = "http://localhost:3000/"
-export const BASE_URL="https://campusconnectbackend-q596.onrender.com/"
+export const BASE_URL = "https://campusconnectbackend-q596.onrender.com/"
+
+
 const API = axios.create({
   baseURL: "https://campusconnectbackend-q596.onrender.com/",
   // baseURL: "http://localhost:3000/",
